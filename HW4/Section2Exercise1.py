48b7ac6580237f0dcbabd133b0dceb0bba6d89c8
Saul <shaul.s@campus.technion.ac.il>
2020-01-18 18:05:53 +0200

    hw4 q2 started

--- HW4/Section2Exercise1.py
+++ HW4/Section2Exercise1.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 from scipy import linalg as la
-import glob
 
 
 def dist(x, x0, y, y0):
@@ -12,45 +11,23 @@ def dist(x, x0, y, y0):
     return np.sqrt((x - x0) ** 2 + (y - y0) ** 2)
 
 
-def computeImageCorners(imgShape, R):
-    """
-    :param imgShape:
-    :param r:
-    :return:
-    """
-    top_left = np.dot(R, np.array([[0], [0], [1]]))
-    bottom_left = np.dot(R, np.array([[imgShape[0]], [0], [1]]))
-    top_right = np.dot(R, np.array([[0], [imgShape[1]], [1]]))
-    bottom_right = np.dot(R, np.array([[imgShape[1]], [imgShape[1]], [1]]))
-
-    x_min = min(top_left[1], bottom_left[1], top_right[1], bottom_right[1])
-    y_min = min(top_left[0], bottom_left[0], top_right[0], bottom_right[0])
-    x_max = max(top_left[1], bottom_left[1], top_right[1], bottom_right[1])
-    y_max = max(top_left[0], bottom_left[0], top_right[0], bottom_right[0])
-
-    m = x_max - x_min
-    n = y_max - y_min
-
-    T = np.eye(3)
-    T[0, -1] = -x_min
-    T[1, -1] = -y_min
-
-    return np.dot(T, R), np.array([n, m]).astype(int)
-
-
 if __name__ == '__main__':
     img = cv2.imread(r'img1.jpg', 1)
-    transformed_img1 = np.zeros(img.shape, dtype='uint8')
-    transformed_img2 = np.zeros(img.shape, dtype='uint8')
-    transformed_img3 = np.zeros(img.shape, dtype='uint8')
-    transformed_img4 = np.zeros(img.shape, dtype='uint8')
+    ### REDUCING IMG TO GET BETTER RUN TIMES ###
+    reduced_img = cv2.GaussianBlur(img[::2, ::2], (5, 5), 1)
+    reduced_img = cv2.GaussianBlur(reduced_img[::2, ::2], (5, 5), 1)
+
+    transformed_img1 = np.zeros(reduced_img.shape, dtype='uint8')
+    transformed_img2 = np.zeros(reduced_img.shape, dtype='uint8')
+    transformed_img3 = np.zeros(reduced_img.shape, dtype='uint8')
+    transformed_img4 = np.zeros(reduced_img.shape, dtype='uint8')
 
     ### 1 ###
-    a1 = 63  # a quarter of image length
-    x1, y1 = 189, 126  # center of image
+    a1 = 24  # a quarter of image length
+    x1, y1 = 47, 31  # center of image
 
-    for i in range(img.shape[1]):
-        for j in range(img.shape[0]):
+    for i in range(reduced_img.shape[1]):
+        for j in range(reduced_img.shape[0]):
             dist1 = dist(i, x1, j, y1)
             if dist1 != 0:
                 R = np.array([[np.cos(a1 / dist1), -np.sin(a1 / dist1), 0], [np.sin(a1 / dist1), np.cos(a1 / dist1), 0],
@@ -59,14 +36,72 @@ if __name__ == '__main__':
                 R = np.eye(3)
             pix = np.array([[i], [j], [1]])
             target_pix = np.dot(la.inv(R), pix).astype(int)  # computing the transformation
-            if (target_pix[0] >= 0) and (target_pix[0] < img.shape[1]) and (target_pix[1] >= 0) and (
-                    target_pix[1] < img.shape[0]):
-                transformed_img1[j, i, :] = img[target_pix[1], target_pix[0]]
+            if (target_pix[0] >= 0) and (target_pix[0] < reduced_img.shape[1]) and (target_pix[1] >= 0) and (
+                    target_pix[1] < reduced_img.shape[0]):
+                transformed_img1[j, i, :] = reduced_img[target_pix[1], target_pix[0]]
 
-    downsized = img[::2, ::2]
+    ### 2 ###
+    a1 = 20
+    x1, y1 = 90, 60
 
-    plt.imshow(downsized)
-    # plt.imshow(img)
+    for i in range(reduced_img.shape[1]):
+        for j in range(reduced_img.shape[0]):
+            dist1 = dist(i, x1, j, y1)
+            if dist1 != 0:
+                R = np.array([[np.cos(a1 / dist1), -np.sin(a1 / dist1), 0], [np.sin(a1 / dist1), np.cos(a1 / dist1), 0],
+                              [0, 0, 1]])
+            else:
+                R = np.eye(3)
+            pix = np.array([[i], [j], [1]])
+            target_pix = np.dot(la.inv(R), pix).astype(int)  # computing the transformation
+            if (target_pix[0] >= 0) and (target_pix[0] < reduced_img.shape[1]) and (target_pix[1] >= 0) and (
+                    target_pix[1] < reduced_img.shape[0]):
+                transformed_img2[j, i, :] = reduced_img[target_pix[1], target_pix[0]]
 
-    # plt.imshow(transformed_img1)
+        ### 3 ###
+    a1 = 10
+    x1, y1 = 25, 15
+
+    for i in range(reduced_img.shape[1]):
+        for j in range(reduced_img.shape[0]):
+            dist1 = dist(i, x1, j, y1)
+            if dist1 != 0:
+                R = np.array([[np.cos(a1 / dist1), -np.sin(a1 / dist1), 0], [np.sin(a1 / dist1), np.cos(a1 / dist1), 0],
+                              [0, 0, 1]])
+            else:
+                R = np.eye(3)
+            pix = np.array([[i], [j], [1]])
+            target_pix = np.dot(la.inv(R), pix).astype(int)  # computing the transformation
+            if (target_pix[0] >= 0) and (target_pix[0] < reduced_img.shape[1]) and (target_pix[1] >= 0) and (
+                    target_pix[1] < reduced_img.shape[0]):
+                transformed_img3[j, i, :] = reduced_img[target_pix[1], target_pix[0]]
+
+        ### 4 ###
+        a1 = 30
+        x1, y1 = 20, 20
+
+        for i in range(reduced_img.shape[1]):
+            for j in range(reduced_img.shape[0]):
+                dist1 = dist(i, x1, j, y1)
+                if dist1 != 0:
+                    R = np.array(
+                        [[np.cos(a1 / dist1), -np.sin(a1 / dist1), 0], [np.sin(a1 / dist1), np.cos(a1 / dist1), 0],
+                         [0, 0, 1]])
+                else:
+                    R = np.eye(3)
+                pix = np.array([[i], [j], [1]])
+                target_pix = np.dot(la.inv(R), pix).astype(int)  # computing the transformation
+                if (target_pix[0] >= 0) and (target_pix[0] < reduced_img.shape[1]) and (target_pix[1] >= 0) and (
+                        target_pix[1] < reduced_img.shape[0]):
+                    transformed_img4[j, i, :] = reduced_img[target_pix[1], target_pix[0]]
+
+    ### PLOTTING ###
+    plt.subplot(2, 2, 1), plt.imshow(transformed_img1)
+    plt.title('alpha = 24, [x0,y0]=[47, 31]'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 2), plt.imshow(transformed_img2)
+    plt.title('alpha = 20, [x0,y0]=[90, 60]'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 3), plt.imshow(transformed_img3)
+    plt.title('alpha = 10, [x0,y0]=[25, 15]'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 4), plt.imshow(transformed_img4)
+    plt.title('alpha = 30, [x0,y0]=[20, 20]'), plt.xticks([]), plt.yticks([])
     plt.show()
--- HW4/Section2Exercise2.py
+++ HW4/Section2Exercise2.py
@@ -1,8 +1,122 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from skimage.util import random_noise
-import glob
+import Reader as rd
+from scipy import linalg as la
+
+
+def computeTransformationMatrix(srcPoints, trgtPoints):
+    """
+    :param srcPoints:
+    :param trgtPoints:
+    :return:
+    """
+    srcPoints = np.reshape(srcPoints.T, (srcPoints.size, 1))
+    trgtPoints = np.reshape(trgtPoints.T, (trgtPoints.size, 1))
+
+    n = srcPoints.shape[0]  # number of observations
+    u = 8  # number of unknowns
+
+    A = np.zeros((n, u))
+    l = trgtPoints
+
+    for i in range(0, n - 1, 2):
+        A[i, 0] = srcPoints[i]
+        A[i, 1] = srcPoints[i + 1]
+        A[i, 2] = 1
+        A[i, 3] = 0
+        A[i, 4] = 0
+        A[i, 5] = 0
+        A[i, 6] = -srcPoints[i] * trgtPoints[i]
+        A[i, 7] = -srcPoints[i + 1] * trgtPoints[i]
+
+        A[i + 1, 0] = 0
+        A[i + 1, 1] = 0
+        A[i + 1, 2] = 0
+        A[i + 1, 3] = srcPoints[i]
+        A[i + 1, 4] = srcPoints[i + 1]
+        A[i + 1, 5] = 1
+        A[i + 1, 6] = -srcPoints[i] * trgtPoints[i + 1]
+        A[i + 1, 7] = -srcPoints[i + 1] * trgtPoints[i + 1]
+
+    X = np.dot(la.inv(np.dot(A.T, A)), np.dot(A.T, l))
+    R = np.array([[float(X[0]), float(X[1]), float(X[2])], [float(X[3]), float(X[4]), float(X[5])],
+                  [float(X[6]), float(X[7]), 1]])
+
+    return R
+
+
+def computeImageCorners(imgShape, R):
+    """
+    :param imgShape: shape of the image
+    :param R: transformation matrix
+    :return: the new image size and new transform matrix
+    """
+    top_left = np.dot(R, np.array([[0], [0], [1]]))
+    bottom_left = np.dot(R, np.array([[0], [imgShape[0]], [1]]))
+    top_right = np.dot(R, np.array([[imgShape[1]], [0], [1]]))
+    bottom_right = np.dot(R, np.array([[imgShape[1]], [imgShape[0]], [1]]))
+
+    x_min = min(top_left[1], bottom_left[1], top_right[1], bottom_right[1])
+    y_min = min(top_left[0], bottom_left[0], top_right[0], bottom_right[0])
+    x_max = max(top_left[1], bottom_left[1], top_right[1], bottom_right[1])
+    y_max = max(top_left[0], bottom_left[0], top_right[0], bottom_right[0])
+
+    cols = int(x_max - x_min)
+    rows = int(y_max - y_min)
+
+    T = np.eye(3)
+    T[0, -1] = int(-x_min)
+    T[1, -1] = int(-y_min)
+
+    return np.dot(T, R), np.array([cols, rows])
+
 
 if __name__ == '__main__':
+    left_img = cv2.imread(r'left_image.jpg', 1)
+    center_img = cv2.imread(r'center_image.jpg', 1)
+    right_img = cv2.imread(r'right_image.jpg', 1)
+    """
+    ### REDUCING IMG TO GET BETTER RUN TIMES ###
+    r_left_img = cv2.GaussianBlur(left_img[::2, ::2], (5, 5), 1)
+    r_left_img = cv2.GaussianBlur(r_left_img[::2, ::2], (5, 5), 1)
+    r_center_img = cv2.GaussianBlur(center_img[::2, ::2], (5, 5), 1)
+    r_center_img = cv2.GaussianBlur(r_center_img[::2, ::2], (5, 5), 1)
+    r_right_img = cv2.GaussianBlur(right_img[::2, ::2], (5, 5), 1)
+    r_right_img = cv2.GaussianBlur(r_right_img[::2, ::2], (5, 5), 1)
+    """
+    ### SPLITTING IMAGES TO 3 CHANNELS ###
+    left_b, left_g, left_r = cv2.split(left_img)
+    center_b, center_g, center_r = cv2.split(center_img)
+    right_b, right_g, right_r = cv2.split(right_img)
+
+    ### READING HOMOLOGUE POINTS ###
+    sourcePoints = rd.Reader.ReadSampleFile(r'left_image.json')
+    targetPoints_center = rd.Reader.ReadSampleFile(r'center_image.json')
+    targetPoints_right = rd.Reader.ReadSampleFile(r'right_image.json')
+
+    ### COMPUTING TRANSFORMATION MATRIX ###
+    R_center = computeTransformationMatrix(targetPoints_center, sourcePoints)
+    R_right = computeTransformationMatrix(targetPoints_right, sourcePoints)
+
+    ### COMPUTING NEW TRANSFORMATION MATRIX + PANORAMA DIMENSIONS ###
+    left_matrix, left_dimension = computeImageCorners(left_b.shape, R_center)
+    right_matrix, right_dimension = computeImageCorners(right_b.shape, R_right)
+
+    pano_cols = max(left_b.shape[1], left_dimension[0], right_dimension[0])
+    pano_rows = max(left_b.shape[0], left_dimension[1], right_dimension[1])
+
+    panorama = np.zeros((pano_rows, pano_cols))
+
+    for i in range(panorama.shape[1]):
+        for j in range(panorama.shape[0]):
+            pix = np.array([[i], [j], [1]])
+            target_pix = np.dot(la.inv(left_matrix), pix).astype(int)
+            if (target_pix[0] >= 0) and (target_pix[0] < left_b.shape[1]) and (target_pix[1] >= 0) and (
+                    target_pix[1] < left_b.shape[0]):
+                panorama[j, i] = left_b[target_pix[1], target_pix[0]]
+
+    plt.imshow(panorama, cmap='gray')
+    plt.show()
+
     print('hi')
